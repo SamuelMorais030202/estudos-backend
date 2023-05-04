70b877be814e3655f6a289714d5a1d0362b22e27
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-05-04 10:24:28 -0300

    Estudos MSC

--- secao04/dia04/src/app.js
+++ secao04/dia04/src/app.js
@@ -1,8 +1,16 @@
 // src/app.js
 const express = require('express');
-const apiCredenciais = require('./middlewares/apiCredenciais');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
+require('express-async-errors');
+
+const apiCredenciais = require('./middlewares/apiCredenciais');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(apiCredenciais);
 
 let nextId = 3;
 const teams = [
@@ -11,31 +19,42 @@ const teams = [
 ];
 
 app.use(express.json());
-app.use(apiCredenciais);
 
 const validateTeam = (req, res, next) => {
-  const requiredProperties = ['nome', 'sigla']; // propriedades obrigatórias
-  // Verifica se o body da requisição possui os stributos esperados
-  if (requiredProperties.every((property) => property in req.body)) {
-    next(); // Chama o próximo middleware
-  } else {
-    res.sendStatus(400); // Ou já responde avisando que deu errado
-  }
+  const { nome, sigla } = req.body;
+
+  if (!nome) return res.status(400).json({ message: "O campo nome é obrigatório" });
+  if (!sigla) return res.status(400).json({ message: "O campo sigla é obrigatório" });
+  next();
 };
+
+const existingId = (req, res, next) => {
+  const id = Number(req.params.id);
+
+  if (teams.some((t) => t.id === id)) {
+    return next();
+  }
+
+  return res.status(404).json({ message: "Time não encontrado" });
+}
 
 app.get('/teams', (req, res) => res.json(teams));
 
-app.get('/teams/:id', (req, res) => {
+app.get('/teams/:id', existingId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    res.json(team);
-  } else {
-    res.sendStatus(404);
-  }
+  res.json(team);
 });
 
 app.post('/teams', validateTeam, (req, res) => {
+  if (
+    // Confere se a sigla proposta está presente nos times autorizados
+    !req.teams.teams.includes(req.body.sigla)
+    // Confere se já existe um time com essa sigla
+    && teams.every((t) => t.sigla !== req.body.sigla)
+  ) {
+    return res.status(402).json({ message: 'Já existe um time com essa sigla' })
+  }
   // O id recebe o nextid(o próximo id)
   // A constante team ira receber uma chave id, e as informações vindas do body
   const team = { id: nextId, ...req.body };
@@ -46,26 +65,20 @@ app.post('/teams', validateTeam, (req, res) => {
   res.status(201).json(team);
 });
 
-app.put('/teams/:id', validateTeam, (req, res) => {
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    const updated = { id, ...req.body };
-    teams.splice(index, 1, updated);
-    res.status(201).json(updated);
-  } else {
-    res.sendStatus(400);
-  }
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
 });
 
-app.delete('/teams/:id', (req, res) => {
+app.delete('/teams/:id', existingId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-  }
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
   res.sendStatus(204);
 });
 
--- secao04/dia04/src/middlewares/apiCredenciais.js
+++ secao04/dia04/src/middlewares/apiCredenciais.js
@@ -4,14 +4,15 @@ module.exports = async function apiCredenciais (req, res, next) {
   const token = req.header('X-API-TOKEN'); // captura o token do cabeçalho
 
   // Lê o conteúdo de ./autodata.json
-  const authData = await fs.readFile('../../autodata.json', { encoding: 'utf-8' });
+  const authData = await fs.readFile('./autodata.json', { encoding: 'utf-8' });
 
   // redFile nos deu uma string, agora vamos carregar o objeto a partir dela 
   const autorized = JSON.parse(authData);
 
   if (token in autorized) {
+    res.teams = autorized[token];
     next(); // pode continuar 
   } else {
-    res.sendStatus(401); // não autorizado
+    res.status(401).jsom({ message: "Token invlído ou expirado" }); // não autorizado
   }
 }
